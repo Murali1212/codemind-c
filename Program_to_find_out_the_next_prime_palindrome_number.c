cc7859d64ea1a9896d6bd4ac5590cb437964d5f1
Murali1212 <99945793+Murali1212@users.noreply.github.com>
2022-02-18 15:14:15 +0530

    #include <bits/stdc++.h> #include <math.h> int main() { long n, t, r = 0, c, d; std::cin>>n; while (1) { 	n++; 	t = n; 	while(t) 	{ 	      r = r*10; 	      r = r + t%10; 	      t = t/10;     	}
    	if (r == n)
    	{
    	      d = (int)sqrt(n);
    	      for (c = 2; c <= d; c++)
    	      {
    	        if (n%c == 0)
    	        break;
          	      }
          	     if (c == d+1)
    	     break;
        	}
        	r = 0;
    }
    
    std::cout<<n<<"
    ";
    return 0;
    }

---
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <math.h>
+int main()
+{
+long n, t, r = 0, c, d;
+std::cin>>n;
+while (1)
+{
+	n++;
+	t = n;
+	while(t)
+	{
+	      r = r*10;
+	      r = r + t%10;
+	      t = t/10;
+    	}
+	
+	if (r == n)
+	{
+	      d = (int)sqrt(n);
+	      for (c = 2; c <= d; c++)
+	      {
+	        if (n%c == 0)
+	        break;
+      	      }
+      	     if (c == d+1)
+	     break;
+    	}
+    	r = 0;
+}
+
+std::cout<<n<<"
+";
+return 0;
+}
+
