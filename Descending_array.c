39872fb9b4b00dd9bf47b79268cbdbcd54e351fe
Murali1212 <99945793+Murali1212@users.noreply.github.com>
2022-02-24 15:29:59 +0530

    #include<bits/stdc++.h> int main() {   int n,x[20],count=0,i;   std::cin>>n;   for(i=0;i<n;i++)     std::cin>>x[i];
      for(i=0;i<n-1;i++)
      {
          if(x[i]<x[i+1])
            {
                std::cout<<"no";
                return 0;
            }
      }
      std::cout<<"yes";
      return 0;
    }

---
+++ Descending_array.c
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+int main()
+{
+  int n,x[20],count=0,i;
+  std::cin>>n;
+  for(i=0;i<n;i++)
+    std::cin>>x[i];
+  
+  for(i=0;i<n-1;i++)
+  {
+      if(x[i]<x[i+1])
+        {
+            std::cout<<"no";
+            return 0;
+        }
+  }
+  std::cout<<"yes";
+  return 0;
+}
